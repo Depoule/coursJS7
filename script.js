57c7ed4da756a8764966b39dba76b0057f1aec80
Depoule <jpoulet02100@gmail.com>
2019-01-23 13:38:31 +0100

    exo 7 fini en attente de correction

---
+++ script.js
@@ -0,0 +1,9 @@
+function getNumber() {
+
+    var shoeSize = document.getElementById('shoeSize').value;
+    var yearOfBirth = document.getElementById('yearOfBirth').value;
+
+    var result = ((((shoeSize * 2) + 5)*50) - yearOfBirth) + 1766;
+
+    alert(result);
+}
