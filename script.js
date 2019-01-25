c051d5247f72bf9236bda37efd1936a4cb0c070d
Depoule <jpoulet02100@gmail.com>
2019-01-25 08:40:00 +0100

    exo 7 fini et corrigé

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-function getNumber() {
+function calcul(){
+  var shoeSize = document.getElementById('shoeSize').value;
+  var yearOfBirth = document.getElementById('yearOfBirth').value;
+  //on vérifie que la saisie utilisateur soient bien des nombre
+  if(isNaN(shoeSize) == false && isNaN(yearOfBirth) == false){
+    // on autorise les pointure entre 34 et 52 et l'année entre 1900 et 2019 <= def: inférieur ou égale >= supérieur ou égale != différent
+    if((shoeSize >= 34 && shoeSize <= 52) && (yearOfBirth >= 1900 && yearOfBirth <= 2019)){
+      var result = shoeSize * 2;
+      result += 5; // result = result + 5;
+      result *= 50;
+      result -= yearOfBirth;
+      result += 1766;
 
-    var shoeSize = document.getElementById('shoeSize').value;
-    var yearOfBirth = document.getElementById('yearOfBirth').value;
-
-    var result = ((((shoeSize * 2) + 5)*50) - yearOfBirth) + 1766;
-
-    alert(result);
+      alert(result);
+    }else{
+      alert('La pointure et la date de naissance ne sont pas correctes');
+    }
+  }else{
+    alert('il faut saisir des chiffres');
+  }
 }
